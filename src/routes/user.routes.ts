f15b8cd1f3a6ca1a8d53621dc4ccaf859fbf0090
RafaelSSantiago <raphasantiago768@gmail.com>
2025-02-15 14:40:17 -0300

    feat: add asyncHandler

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -18,26 +18,41 @@ router.post(
   })
 );
 
-router.post("/signin", async (req: Request, res: Response) => {
-  UserController.loginUser(req, res);
-});
+router.post(
+  "/signin",
+  asyncHandler(async (req: Request, res: Response) => {
+    UserController.loginUser(req, res);
+  })
+);
 
 router.use(authMiddleware);
 
-router.get("/", async (req: Request, res: Response) => {
-  UserController.getUserById(req, res);
-});
+router.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response) => {
+    UserController.getUserById(req, res);
+  })
+);
 
-router.get("/:id", async (req: Request, res: Response) => {
-  UserController.getUserById(req, res);
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    UserController.getUserById(req, res);
+  })
+);
 
-router.patch("/:id", async (req: Request, res: Response) => {
-  UserController.updateUser(req, res);
-});
+router.patch(
+  "/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    UserController.updateUser(req, res);
+  })
+);
 
-router.delete("/:id", async (req: Request, res: Response) => {
-  void UserController.deleteUser(req, res);
-});
+router.delete(
+  "/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    void UserController.deleteUser(req, res);
+  })
+);
 
 export default router;
